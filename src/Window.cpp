f9a1618ee4eceeb8a03bd9210c1acd288e8ebc45
sdroppelmann <steffen.droppelmann@gmail.com>
2021-10-07 15:50:25 +0200

    added persistent window config and small style adjustments

--- src/Window.cpp
+++ src/Window.cpp
@@ -73,6 +73,8 @@ namespace vOS
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
+        io.IniFilename = "./res/config.ini";
+
         // Setup Dear ImGui style
         ImGui::StyleColorsDark();
 
@@ -81,8 +83,23 @@ namespace vOS
         ImGui_ImplOpenGL3_Init(glsl_version);
 
         io.Fonts->AddFontFromFileTTF("./res/fonts/Roboto-Medium.ttf", 18.0f);
-        ImGui::GetStyle().FrameRounding = 4.0f;
-        ImGui::GetStyle().GrabRounding = 4.0f;
+        ImGuiStyle& style = ImGui::GetStyle();
+        style.FrameRounding = 4.0f;
+        style.GrabRounding = 4.0f;
+        style.FramePadding = {10.0f, 6.0f};
+        style.ItemSpacing = {16.0f, 5.0f};
+        style.IndentSpacing = 30.0f;
+        style.ScrollbarSize = 20.0f;
+        style.WindowBorderSize = 0.0f;
+        style.ChildBorderSize = 0.0f;
+        style.PopupBorderSize = 0.0f;
+        style.WindowRounding = 12.0f;
+        style.ChildRounding = 12.0f;
+        style.FrameRounding = 12.0f;
+        style.PopupRounding = 12.0f;
+        style.ScrollbarRounding = 12.0f;
+        style.WindowTitleAlign = {0.5f, 0.5f};
+
 
         ImVec4* colors = ImGui::GetStyle().Colors;
         colors[ImGuiCol_Text] = ImVec4(0.95f, 0.96f, 0.98f, 1.00f);
@@ -152,8 +169,96 @@ namespace vOS
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
+            // Note: Switch this to true to enable dockspace
+            static bool dockspaceOpen = true;
+            static bool opt_fullscreen_persistant = true;
+            bool opt_fullscreen = opt_fullscreen_persistant;
+            static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+            // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+            // because it would be confusing to have two docking targets within each others.
+            ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+            if (opt_fullscreen)
+            {
+                ImGuiViewport* viewport = ImGui::GetMainViewport();
+                ImGui::SetNextWindowPos(viewport->Pos);
+                ImGui::SetNextWindowSize(viewport->Size);
+                ImGui::SetNextWindowViewport(viewport->ID);
+                ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+                ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+                window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+                window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+            }
+
+            // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
+            if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+                window_flags |= ImGuiWindowFlags_NoBackground;
+
+            // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+            // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+            // all active windows docked into it will lose their parent and become undocked.
+            // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+            // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+            ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
+            ImGui::PopStyleVar();
+
+            if (opt_fullscreen)
+                ImGui::PopStyleVar(2);
+
+            // DockSpace
+            ImGuiIO& io = ImGui::GetIO();
+            ImGuiStyle& style = ImGui::GetStyle();
+            float minWinSizeX = style.WindowMinSize.x;
+            style.WindowMinSize.x = 370.0f;
+            if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+            {
+                ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+                ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+            }
+
+            style.WindowMinSize.x = minWinSizeX;
+
+            if (ImGui::BeginMenuBar())
+            {
+                if (ImGui::BeginMenu("File"))
+                {
+                    if (ImGui::MenuItem("New", "Ctrl+N"))
+                    {}
+
+                    if (ImGui::MenuItem("Open...", "Ctrl+O"))
+                    {}
+
+                    if (ImGui::MenuItem("Save As...", "Ctrl+Shift+S"))
+                    {}
+
+                    if (ImGui::MenuItem("Exit"))
+                    {}
+                    ImGui::EndMenu();
+                }
+                if (ImGui::BeginMenu("Edit"))
+                {
+                    if (ImGui::MenuItem("New", "Ctrl+N"))
+                    {}
+
+                    if (ImGui::MenuItem("Open...", "Ctrl+O"))
+                    {}
+
+                    if (ImGui::MenuItem("Save As...", "Ctrl+Shift+S"))
+                    {}
+
+                    if (ImGui::MenuItem("Exit"))
+                    {}
+                    ImGui::EndMenu();
+                }
+
+                ImGui::EndMenuBar();
+            }
+
             if (show_demo_window)
                 ImGui::ShowDemoWindow(&show_demo_window);
+
+            ImGui::End();
 
             // Rendering
             ImGui::Render();
