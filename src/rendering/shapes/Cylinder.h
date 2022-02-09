c035a0d5f4e900c51acec39568284155f9163ed6
Max König <maxkoenig@uos.de>
2022-02-09 17:29:33 +0100

    Refactored All Shape-Classes

--- src/rendering/shapes/Cylinder.h
+++ src/rendering/shapes/Cylinder.h
@@ -10,10 +10,14 @@ namespace vOS
     public:
 
         ~Cylinder() override;
+
+        /**
+         * draw method that overrides the draw-method of shape to draw a cylinder
+         */
         void draw() override;
 
     private:
-
+        // holds the vertices of the box
         VertexArrayObject* m_vao;
     };
 }
