5d9dcc305d0df8b94d0d5ac8c2675c8a214d728b
Steffen Droppelmann <steffen.droppelmann@gmail.com>
2022-02-23 10:47:46 +0100

    changed opengl version for apple devices

--- src/ImguiRenderer.cpp
+++ src/ImguiRenderer.cpp
@@ -71,7 +71,7 @@ namespace vOS
         // GL 3.2 + GLSL 150
         m_glslVersion = "#version 150";
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
 #else
