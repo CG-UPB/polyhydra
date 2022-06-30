be11a141c2fe7d08aaf612fb4b5e3882fee5c5c1
sdroppelmann <steffen.droppelmann@gmail.com>
2022-06-30 19:23:13 +0200

    removed RenderData since it is unnecessary

--- src/rendering/passes/RenderPass.h
+++ src/rendering/passes/RenderPass.h
@@ -8,26 +8,27 @@
 #include "../gl/Shader.h"
 #include "rendering/camera/Camera.h"
 #include "../Light.h"
+#include "rendering/Renderer.h"
 
 namespace volumeshOS::Internal
 {
-    /**
-     * Render Data includes World Information such as the Camera and Lighting
-     */
-    struct RenderData
-    {
-        Camera camera;
-        Light light;
-    };
-
     class RenderPass
     {
+    public:
+
+        explicit RenderPass(Renderer* renderer): m_renderer(renderer)
+        {}
+
         /**
          * Renders the VAO using render data and mesh data as parameters
          * @param vao
          * @param render_data
          * @param mesh_id
          */
-        virtual void render(std::shared_ptr<VertexArrayObject> vao, const RenderData& render_data, std::shared_ptr<MeshObject> mesh) = 0;
+        virtual void render(std::shared_ptr<VertexArrayObject> vao, std::shared_ptr<MeshObject> mesh) = 0;
+
+    protected:
+
+        Renderer* m_renderer = nullptr;
     };
 }
